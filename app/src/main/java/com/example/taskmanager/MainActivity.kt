30832ff59dd1b1b353a5fd41f83ffaa4dc8f5f9a
2s494 <yodgorovasror37@gmail.com>
2026-05-26 19:27:18 +0500

    state managment o'tildi

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -3,13 +3,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,29 +29,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TaskManagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TaskListScreen(
+
+                )
             }
         }
     }
 }
-
+data class Task(val title : String, val isDone : Boolean, val priority : Int)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TaskListScreen() {
+    val tasks = listOf<Task>(Task("Kotlin o'rganish", true, 1),
+        Task("Swift o'rganish", false, 2),
+        Task("Rive o'rganish", false, 3),
+        Task("Backend ga kirish", false, 4),
+        Task("Software ingineer bo'lish", false, 5),
+        )
+    LazyColumn {
+        items(tasks){ task ->
+            TaskItem(task)
+        }
+    }
 }
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TaskManagerTheme {
-        Greeting("Android")
+fun TaskItem(item : Task){
+    var isDone by remember { mutableStateOf(item.isDone) }
+    Card (
+        modifier =  Modifier.fillMaxWidth().padding(8.dp)
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = item.title, modifier = Modifier.padding(16.dp).background(Color.Yellow))
+            Checkbox(checked = isDone, onCheckedChange = {
+                isDone = it
+            })
+        }
     }
 }
